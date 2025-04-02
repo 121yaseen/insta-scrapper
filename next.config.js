54da29e4bef7856930068b1752f9bf8ab71392fa
Yaseen <121yaseen@gmail.com>
2025-04-02 12:12:24 +0530

    Enhance Next.js configuration with Webpack alias and update package.json for postinstall script and additional dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  /* config options here */ webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
