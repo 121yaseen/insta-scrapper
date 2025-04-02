5aaefa8ab3545cf9301d896c0427d6c8ec5c648f
Yaseen <121yaseen@gmail.com>
2025-04-02 12:16:31 +0530

    Refactor Next.js configuration to use ES module syntax and improve path resolution

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig = {
-  /* config options here */ webpack: (config) => {
+  webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
   },
